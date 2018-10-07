4d923f91a9799d80aa9d9c435b28912ffc75e978
takashiAg <crawd4274@gmail.com>
2018-10-08 04:29:27 +0900

    second commit

--- ESP_servo.cpp
+++ ESP_servo.cpp
@@ -1,14 +1,18 @@
 #include "ESP_servo.h"
 #include "Arduino.h"
 
-ESP_servo::ESP_servo() {
+ESP_servo::ESP_servo()
+{
 }
-void ESP_servo:: init(uint8_t pin, uint8_t channel) {
+void ESP_servo::init(uint8_t pin, uint8_t channel)
+{
   CHANNEL = channel;
 
   //PWM freqency [Hz]
   uint16_t freqency = 50;
-  //range of led controller
+  servo_0 = 0.0005f * freqency * 1024;
+  servo_180 = 0.0024f * freqency * 1024;
+
   uint8_t bit = 10;
 
   ledcSetup(CHANNEL, freqency, bit);
@@ -16,7 +20,8 @@ void ESP_servo:: init(uint8_t pin, uint8_t channel) {
   ledcAttachPin(pin, CHANNEL);
 }
 
-void ESP_servo:: write(uint16_t degree) {
+void ESP_servo::write(uint16_t degree)
+{
   int value = map(degree, 0, 180, servo_0, servo_180);
   int Normalized_value = constrain(value, min, max);
   //ledcWrite(channel, value);
--- ESP_servo.h
+++ ESP_servo.h
@@ -7,23 +7,20 @@
 
 // (26/1024)*20ms ≒ 0.5 ms  (-90°)
 // (123/1024)*20ms ≒ 2.4 ms (+90°)
-#define servo_0  26
-#define servo_180  123
+#define servo_0_default 26
+#define servo_180_default 123
 
+class ESP_servo
+{
+public:
+  ESP_servo();
+  void init(uint8_t pin, uint8_t channel);
+  void write(uint16_t degree);
+  uint16_t servo_0 = servo_0_default, servo_180 = servo_180_default;
 
-class ESP_servo {
-  public:
-    ESP_servo();
-    void init(uint8_t pin, uint8_t channel);
-    void write(uint16_t degree);
-
-  private:
-    uint8_t min = 10, max = 170;
-    uint8_t CHANNEL;
+private:
+  uint8_t min = 10, max = 170;
+  uint8_t CHANNEL;
 };
 
-
 #endif
-
-
-
